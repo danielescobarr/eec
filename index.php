3500c63f49ee05ee73e6a2b815271f78e35293bf
Daniel Escobar Rodríguez <66046293+danielescobarr@users.noreply.github.com>
2023-11-20 15:52:31 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Mantención</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Mantención</h1>
+  </header>
+  <main>
+    <p>
+      Estamos en mantención. Volveremos pronto.
+    </p>
+    <div class="redes-sociales">
+      <a href="https://www.instagram.com/" target="_blank">
+        <img src="img/instagram.svg" alt="Instagram">
+      </a>
+      <a href="https://www.facebook.com/" target="_blank">
+        <img src="img/facebook.svg" alt="Facebook">
+      </a>
+      <a href="https://twitter.com/" target="_blank">
+        <img src="img/twitter.svg" alt="Twitter">
+      </a>
+    </div>
+  </main>
+  <footer>
+    <p>
+      Copyright © 2023
+    </p>
+  </footer>
+</body>
+</html>
